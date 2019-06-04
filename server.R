bd7ba32ea059d721804c8a369122708ebed3bc42
Jacinta Harshe <jharshe@uw.edu>
2019-06-03 23:16:08 -0700

    Created question 3, started histogram

--- server.R
+++ server.R
@@ -5,12 +5,24 @@ movies <- read.csv("data/movies.csv")
 
 shinyServer(
   function(input, output) {
+    
     output$genreRate <- renderPlot(
       ggplot(movies, aes(x = movies$year, y = movies$averageRating)) +
         geom_point()
-        
-      
     )
   
+    output$distPlot <- renderPlot(
+      x    <- movies$average_age = integer(average_age),
+      bins <- seq(min(x), max(x), length.out = input$bins + 1),
+      
+      hist(x, breaks = bins, col = "#75AADB", border = "white",
+           xlab = "Average rating",
+           main = "Histogram of average movie ratings based on average actor age"
+           
+    )
+    )  
+      
   }
+
+  
 )
--- ui.R
+++ ui.R
@@ -4,8 +4,12 @@
 
 # 2. User chooses the genre of interest and sees a SCATTERPLOT
 #    of a year vs average Rating
+
+# 3. User chooses the average actor age on a slider and sees a HISTOGRAM
+#   of average rating on x-axis and frequency on y-axis.
+#
 # grepl(genres, "string of single genre")
-# GET KEAGAN TO GO THROUGH GENRES ON THE CSV TO MAKE SURE I HAVE ALL THE OPTIONS AVAILABLE
+
 
 library(shiny)
 movies <- read.csv("data/movies.csv")
@@ -19,22 +23,31 @@ shinyUI(
       
       # All the genres that a user can choose
       sidebarPanel(
-        selectInput("Genre", "Choose a genre", choices = c("Action","Adventure","Sci-Fi","Drama",
-                                                         "Horror","Thriller","Mystery","Crime",
-                                                         "Biography","Comedy","Animation","Fantasy",
-                                                         "Action","War","Romance","Family","History",
-                                                         "Musical"))
+        selectInput("Genre", "Choose a genre", choices = c("Action", "Adventure", "Animation","Biography", 
+                                                           "Comedy", "Crime", "Documentary", "Drama", "Family",
+                                                           "Fantasy", "Film-Noir", "History", "Horror",
+                                                           "Musical", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller",
+                                                           "War", "Western"))
       ),
       
       # All the release years that a user can choose
       sidebarPanel(
         selectInput("Year", "Choose a year", choices = c(1911:2019))
-      )
+      ),
+      
+      # The minimum and maximum average actor ages that a user can choose
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 5,
+                  max = 95.5,
+                  value = 30)
+      ),
       
       mainPanel(
         p("This app is made by movie lovers for movie lovers! Here you can learn about.....YADAYADAYDADA"),
         plotOutput("genreRate") # Scatter plot
         plotOutput("yearRate")  # Barplot 
+        plotOutput(outputId = "distPlot") # Histogram
       )
     )
   )
