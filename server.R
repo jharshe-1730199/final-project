50f5b7029b2d10520839b2e54a887f8f64f1c439
Jacinta Harshe <jharshe@uw.edu>
2019-06-04 14:42:30 -0700

    committed changes to merge

--- server.R
+++ server.R
@@ -14,20 +14,6 @@ shinyServer(
          xlim(1911, 2019) +
          ylim(1, 10)
     )
-  
-    #output$distPlot <- renderPlot(
-    #  x    <- movies$average_age = integer(average_age),
-    #  bins <- seq(min(x), max(x), length.out = input$bins + 1),
-      
-    #  hist(x, breaks = bins, col = "#75AADB", border = "white",
-    #       xlab = "Average rating",
-    #       main = "Histogram of average movie ratings based on average actor age"
-    #  )
-    #)  
-    #  ggplot(movies, aes(x = year, y = averageRating)) +
-    #    geom_point(data = movies %>%
-    #                      filter(toString(input$Genre) == genres)) +
-    #    xlim(1911, 2019) +
-    #    ylim(1, 10)
+
   }
 )
