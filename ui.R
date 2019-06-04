c4bf14c18b62d29976ed7811f744dfb90f58caf5
Keagan Anderson <keagan39@yahoo.com>
2019-06-04 13:55:05 -0700

    Tabs Added

--- ui.R
+++ ui.R
@@ -8,36 +8,51 @@
 
 library(shiny)
 movies <- read.csv("data/movies.csv")
-
 shinyUI(
-  fluidPage(
-    titlePanel("IMDB Data"),
-    
-    sidebarLayout(
-      position = "right",
-      
-      # All the genres that a user can choose
-      sidebarPanel(
-        selectInput("Genre", "Choose a genre", choices = c("Action","Adventure","Animation","Biography",
-                                                         "Comedy","Crime","Documentary","Drama","Family",
-                                                         "Fantasy","History","Horror","Musical","Mystery",
-                                                         "Romance","Sci-Fi","Thriller","War","Western",
-                                                         "Action,Adventure","Action,Comedy","Action,Sci-Fi",
-                                                         "Comedy,Drama","Drama,Romance","Documentary,Horror",
-                                                         "Horror,Romance","Horror,Thriller","Musical,Romance"
-                                                         )
-        )
-      ),
-
-      # All the release years that a user can choose
-#      sidebarPanel(
-#        selectInput("Year", "Choose a year", choices = c(1911:2019))
-#      ),
-      
+  navbarPage(
+    "IMDB Data",
+    tabPanel(
+      "Home",
       mainPanel(
-      #  p("This app is made by movie lovers for movie lovers! Here you can learn about.....YADAYADAYDADA"),
-        plotOutput("genreRate") # Scatter plot
-      #  plotOutput("yearRate")  # Barplot 
+        
+      )
+    ),
+    
+    tabPanel(
+      "1st Plot"
+    ),
+    
+    tabPanel(
+      "2nd Plot"
+    ),
+    
+    tabPanel(
+      "3rd Plot"
+    ),
+    
+    tabPanel(
+      "Ratings ScatterPlot",
+    
+      sidebarLayout(
+        position = "right",
+        
+        # All the genres that a user can choose
+        sidebarPanel(
+          selectInput("Genre", "Choose a genre", choices = c("Action","Adventure","Animation","Biography",
+                                                           "Comedy","Crime","Documentary","Drama","Family",
+                                                           "Fantasy","History","Horror","Musical","Mystery",
+                                                           "Romance","Sci-Fi","Thriller","War","Western",
+                                                           "Action,Adventure","Action,Comedy","Action,Sci-Fi",
+                                                           "Comedy,Drama","Drama,Romance","Documentary,Horror",
+                                                           "Horror,Romance","Horror,Thriller","Musical,Romance"
+                                                           )
+          )
+        ),
+        mainPanel(
+        #  p("This app is made by movie lovers for movie lovers! Here you can learn about.....YADAYADAYDADA"),
+          plotOutput("genreRate") # Scatter plot
+        #  plotOutput("yearRate")  # Barplot 
+        )
       )
     )
   )
